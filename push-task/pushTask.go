909923b7335e58ad67ce2a2f40928d2095d22e0d
luexp <10457192@qq.com>
2023-12-06 12:17:50 +0800

    add pushtask test

--- push-task/pushTask.go
+++ push-task/pushTask.go
@@ -2,7 +2,6 @@ package push_task
 
 import (
 	"log"
-	"rssbot/bot"
 	"rssbot/db"
 	"rssbot/rsspull"
 	"rssbot/rsspull/parse"
@@ -12,13 +11,18 @@ import (
 )
 
 type PushTask struct {
-	msgCh    chan *bot.PushMsg
+	msgCh    chan *PushMsg
 	wg       sync.WaitGroup
 	once     sync.Once
 	shutdown chan struct{}
 }
 
-func NewPushTask(msgCh chan *bot.PushMsg) *PushTask {
+type PushMsg struct {
+	TwitterId int64
+	Info      *parse.FeedInfo
+}
+
+func NewPushTask(msgCh chan *PushMsg) *PushTask {
 	return &PushTask{msgCh: msgCh, shutdown: make(chan struct{})}
 }
 
@@ -46,20 +50,21 @@ func (p *PushTask) Start() {
 
 						items := make([]*parse.FeedItem, 0)
 						for _, item := range feed.Items {
-							if num, err := strconv.ParseInt(item.PubDate, 10, 64); err == nil && num >= user.PrevSendTime {
+							if num, err := strconv.ParseInt(item.PubDate, 10, 64); err == nil && num >= user.PrevPullTime {
 								items = append(items, item)
 							}
 						}
 						feed.Items = items
 
-						p.msgCh <- &bot.PushMsg{TwitterId: user.TwitterId, Info: feed}
-						user.PrevSendTime = time.Now().Unix()
+						p.msgCh <- &PushMsg{TwitterId: user.TwitterId, Info: feed}
 					}
+
+					user.PrevPullTime = time.Now().Unix()
 				}(user)
 			}
 			// save
 			p.wg.Wait()
-			db.UpdateUsers(users)
+			db.UpdateUsersPrevPullTime(users)
 		}
 	}
 }
---
+++ push-task/pushTask_test.go
@@ -0,0 +1,53 @@
+package push_task
+
+import (
+	"rssbot/db"
+	"testing"
+	"time"
+)
+
+func dbInit() {
+	// clear
+	db.Db.Where("1 = 1").Delete(&db.User{})
+	db.Db.Where("1 = 1").Delete(&db.Task{})
+
+	t := &db.Task{
+		TaskId:    1,
+		StartTime: 0,
+		Period:    1,
+	}
+	db.CreateTask(t)
+
+	u := &db.User{
+		TwitterId:    111,
+		Url:          "https://www.ruanyifeng.com/blog/atom.xml",
+		TaskId:       1,
+		PrevPullTime: 0,
+	}
+	db.CreateUser(u)
+}
+
+func TestDb(t *testing.T) {
+	//runtime.GOMAXPROCS(1)
+	dbInit()
+	ch := make(chan *PushMsg)
+	push := NewPushTask(ch)
+	go push.Start()
+
+	after := time.After(10 * time.Second)
+
+	for {
+		select {
+		case <-after:
+			push.Close()
+			time.Sleep(1 * time.Second)
+			return
+		case msg := <-ch:
+			t.Log(msg.TwitterId)
+			t.Log(msg.Info.Channel.Title, msg.Info.Channel.Title)
+			for _, i := range msg.Info.Items {
+				t.Log(i.Title, i.Link, i.PubDate)
+			}
+		}
+	}
+}
