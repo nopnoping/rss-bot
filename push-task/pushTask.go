ac54d71d043f4b1f04df3931dc1c26f2286f652a
luexp <10457192@qq.com>
2023-12-07 14:50:11 +0800

    Optimize PushTask

--- push-task/pushTask.go
+++ push-task/pushTask.go
@@ -50,9 +50,18 @@ func (p *PushTask) Start() {
 				break
 			}
 
+			// use url to group
+			urlPull := make(map[string][]*db.User)
 			for _, user := range users {
+				if _, ok := urlPull[user.Url]; !ok {
+					urlPull[user.Url] = make([]*db.User, 0)
+				}
+				urlPull[user.Url] = append(urlPull[user.Url], user)
+			}
+
+			for url, users := range urlPull {
 				p.wg.Add(1)
-				go func(user *db.User) {
+				go func(url string, users []*db.User) {
 					defer func() {
 						if r := recover(); r != nil {
 							fmt.Printf("push task handle pull err:%v\n", r)
@@ -60,23 +69,28 @@ func (p *PushTask) Start() {
 					}()
 					defer p.wg.Done()
 
-					if feed := rsspull.GetDefaultRssPull().Pull(user.Url); feed != nil {
-
-						items := make([]*parse.FeedItem, 0)
-						for _, item := range feed.Items {
-							if num, err := strconv.ParseInt(item.PubDate, 10, 64); err == nil && num >= user.PrevPullTime {
-								items = append(items, item)
+					if feed := rsspull.GetDefaultRssPull().Pull(url); feed != nil {
+						for _, user := range users {
+							items := make([]*parse.FeedItem, 0)
+							f := &parse.FeedInfo{
+								Channel: feed.Channel,
+								Items:   items,
 							}
-						}
-						feed.Items = items
-
-						if len(feed.Items) > 0 {
-							p.msgCh <- &PushMsg{ChatId: user.ChatId, Info: feed}
+							for _, item := range feed.Items {
+								if num, err := strconv.ParseInt(item.PubDate, 10, 64); err == nil && num >= user.PrevPullTime {
+									items = append(items, item)
+								}
+							}
+							if len(items) > 0 {
+								p.msgCh <- &PushMsg{ChatId: user.ChatId, Info: f}
+							}
 						}
 					}
 
-					user.PrevPullTime = time.Now().Unix()
-				}(user)
+					for _, user := range users {
+						user.PrevPullTime = time.Now().Unix()
+					}
+				}(url, users)
 			}
 			// save
 			p.wg.Wait()
